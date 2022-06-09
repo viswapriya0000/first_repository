3d8ce4fe9946f716a9e821ce1008a6d2e80f1a6b
viswapriya0000 <viswapriya0000@gmail.com>
2022-06-09 20:16:25 +0530

    second commit

--- simpleproject/src/main/java/com/example/controller/Controller.java
+++ simpleproject/src/main/java/com/example/controller/Controller.java
@@ -3,12 +3,25 @@ package com.example.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping("/enter")
+@RequestMapping
 public class Controller {
-	@GetMapping
-	public String hello() {
+	@GetMapping("/docker")
+	public String docker() {
 		return "hello docker";
 	}
+	
+	@GetMapping("/kubernetes")
+	public String kubernetes() {
+		return "hello kubernetes";
+	}
+	
+	@GetMapping("/gcp")
+	public String gcp() {
+		return "hello GCP";
+	}
+
+
 
 }
